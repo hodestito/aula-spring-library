32f6ef44fddfe60b9cd0fa134a8571e91bf90d1f
Logon Aluno <logonaluno@local.com>
2020-01-22 22:40:25 -0300

    Testes

--- src/test/java/br/com/fiap/library/BookControllerTest.java
+++ src/test/java/br/com/fiap/library/BookControllerTest.java
@@ -2,16 +2,21 @@ package br.com.fiap.library;
 
 import br.com.fiap.library.controller.BookController;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
+import org.junit.jupiter.api.Assertions.*;
 
 public class BookControllerTest {
 
+    BookController bookController = new BookController();
+
     @Test
     public void getAllTest(){
-        BookController bookController = new BookController();
+        //Assert.notEmpty(bookController.getAll("Senhor dos Aneis"));
+        assertEquals(bookController.getAll("Senhor dos Aneis"));
+    }
 
-        Assert.notEmpty(bookController.getAll("Senhor dos Aneis"));
-
+    @Test
+    public void getByIdTest(){
+        Assert.notEmpty(bookController.findById(1));
     }
 
 }
