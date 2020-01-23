50b3012cf2ebd51f52a541492c873b3e6eeef9cb
Logon Aluno <logonaluno@local.com>
2020-01-22 22:21:09 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/library/dto/CreateBookDTO.java
@@ -0,0 +1,53 @@
+package br.com.fiap.library.dto;
+
+import java.time.ZonedDateTime;
+
+public class CreateBookDTO {
+
+    private String titulo;
+    private Integer quantidadeDePaginas;
+    private String ISBN;
+    private ZonedDateTime dataLancamento;
+
+    public CreateBookDTO() {
+    }
+
+    public CreateBookDTO(String titulo, Integer quantidadeDePaginas, String ISBN, ZonedDateTime dataLancamento) {
+        this.titulo = titulo;
+        this.quantidadeDePaginas = quantidadeDePaginas;
+        this.ISBN = ISBN;
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getQuantidadeDePaginas() {
+        return quantidadeDePaginas;
+    }
+
+    public void setQuantidadeDePaginas(Integer quantidadeDePaginas) {
+        this.quantidadeDePaginas = quantidadeDePaginas;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public ZonedDateTime getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(ZonedDateTime dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+}
